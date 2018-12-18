7161122fce700e8967faf408b541ffb7931ea272
flynn.yu <flynn.yu@ucloud.cn>
2018-12-18 14:36:06 +0800

    count remove group

--- example/exists.js
+++ example/exists.js
@@ -4,6 +4,8 @@ const run = async () => {
   const conn = await db.createConn();
   const result = await conn.exists('tb_example', {
     name: 'superdb1d',
+  }, {
+    group: ['name desc'],
   });
   console.log(result);
 };
